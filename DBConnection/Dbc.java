765322a851bbe691fba7111f9ece97421e3318ef
AsmaSulthana345 <shaikasmasulthana01@gmail.com>
2026-07-16 14:17:36 +0530

    feat: establish database connectivity layer
    Add DB Connection class (Dbc) utilizing JDBC driver setups.

---
+++ DBConnection/Dbc.java
@@ -0,0 +1,31 @@
+package com.DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Dbc {
+	
+		public static Connection getConnection() {
+			Connection con = null;
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Organization","root","root");
+				
+			}
+			
+			catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return con;
+
+		}
+
+	}
